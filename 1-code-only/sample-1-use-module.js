63931a7ded90859329bcb43392c8e85bc2d93ff9
Josh Cunningham <josh@joshcanhelp.com>
2020-11-14 17:14:58 -0800

    Progress

---
+++ 1-code-only/sample-1-use-module.js
@@ -0,0 +1,20 @@
+// First, install the `jsonwebtoken` package ([npmjs.com](https://www.npmjs.com/package/jsonwebtoken)):
+//
+// ```
+// $ npm install jsonwebtoken
+// ```
+//
+// Require it at the top of your module:
+//
+const jwt = require("jsonwebtoken");
+
+// The session token payload includes claims typical of an OpenID Connect ID token.
+const payload = {
+    aud: "AUDIENCE_ID",
+    iss: `https://example.com/`,
+    sub: "SUBJECT_ID"
+};
+
+jwt.sign(payload, this.tokenSecret, {
+    expiresIn: this.tokenExpiresIn,
+});
